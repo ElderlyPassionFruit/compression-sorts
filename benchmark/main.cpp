4c8afe8a35de3a12f473efbf50cc6da76e7c5e5a
Igor Markelov <ismarkelov@edu.hse.ru>
2024-04-21 18:37:45 +0000

    Add first version of benchmark stand usage

---
+++ benchmark/main.cpp
@@ -0,0 +1,22 @@
+#include "compression_sorts/benchmark.hpp"
+#include "compression_sorts/get_all_files.hpp"
+#include "compression_sorts/identity.hpp"
+#include "compression_sorts/interfaces.hpp"
+#include "compression_sorts/statistics_saver.hpp"
+
+using namespace CompressionSorts;
+
+template <typename T>
+void TestAllBenchmarksWithAlgorithm(std::filesystem::path dir,
+                                    const CompressionSorts::IPermute<T>& algoritm) {
+    auto paths = GetAllFiles(dir);
+    for (const auto& path : paths) {
+        auto benchmark_results = TestAlgorithm(path, algoritm, 1);
+        SaveBenchmarkResults(benchmark_results);
+    }
+}
+
+int main() {
+    CompressionSorts::IdentityPermutation<int> identity;
+    TestAllBenchmarksWithAlgorithm<int>("test_data/int", identity);
+}
