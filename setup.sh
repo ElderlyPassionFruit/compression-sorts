ec6f8124b7fe41bd7379e3761869ce6ff3827cee
Igor Markelov <ismarkelov@edu.hse.ru>
2024-05-01 17:20:20 +0000

    Add tests

--- setup.sh
+++ setup.sh
@@ -4,6 +4,19 @@ sudo apt upgrade -y
 
 sudo apt install python3 python3-dev git wget unzip acl build-essential libssl-dev libffi-dev liblz4-dev -y
 
+GTEST_VERSION=1.12.1
+
+rm -rf googletest
+git clone https://github.com/google/googletest.git -b release-${GTEST_VERSION} \
+    && cd googletest \
+    && mkdir build \
+    && cd build \
+    && cmake .. \
+    && sudo make install -j \
+    && cd .. \
+    && cd .. \
+    && rm -rf googletest
+
 rm -rf build/ 
 mkdir build/
 cd build
