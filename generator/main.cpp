aabe9f8ab6e233e90e6765039f59d426a0cb9672
Igor Markelov <ismarkelov@edu.hse.ru>
2024-05-05 18:01:46 +0000

    Add unique optimization sorting algorithm

--- generator/main.cpp
+++ generator/main.cpp
@@ -10,6 +10,8 @@
 
 #include "compression_sorts/path.hpp"
 
+std::mt19937_64 rnd(179);
+
 std::vector<size_t> GenBatches(size_t max_batch_size, double exponent) {
     std::vector<size_t> batches;
     for (size_t new_batch_size = 1; new_batch_size <= max_batch_size;
@@ -28,14 +30,23 @@ void InitDirectory(CompressionSorts::Path dir) {
 
 using RawGenerator = std::function<std::string()>;
 
-void GenerateTest(CompressionSorts::Path dir, size_t test_size, RawGenerator raw_generator) {
-    const auto path = dir / (std::to_string(test_size) + ".csv");
-    std::cout << "GenerateTest -" << " dir: " << dir << " test_size: " << test_size << std::endl;
+void SaveTest(CompressionSorts::Path path, const std::vector<std::string>& test) {
     std::ofstream out(path);
     out.tie(0);
-    for (size_t raw = 0; raw < test_size; ++raw) {
-        out << raw_generator() << '\n';
+    for (const auto& raw : test) {
+        out << raw << "\n";
     }
+}
+
+void GenerateTest(CompressionSorts::Path dir, size_t test_size, RawGenerator raw_generator) {
+    std::cout << "GenerateTest -" << " dir: " << dir << " test_size: " << test_size << std::endl;
+    const auto path = dir / (std::to_string(test_size) + ".csv");
+    std::vector<std::string> test(test_size);
+    for (size_t raw = 0; raw < test_size; ++raw) {
+        test[raw] = raw_generator();
+    }
+    std::shuffle(test.begin(), test.end(), rnd);
+    SaveTest(path, test);
 }
 
 void GenerateTests(CompressionSorts::Path dir, const std::vector<size_t>& tests_sizes,
@@ -63,14 +74,13 @@ void GenerateSplitBatches(CompressionSorts::Path dir, const std::vector<size_t>&
 }
 
 int main() {
-    std::mt19937_64 rnd(179);
 
     // random small integers
     {
         constexpr size_t kMaxBatchSize = 1000000;
         const auto batches = GenBatches(kMaxBatchSize, 1.2);
         std::uniform_int_distribution<> distribution(-100, 100);
-        auto raw_generator = [&rnd, &distribution]() -> std::string {
+        auto raw_generator = [&distribution]() -> std::string {
             return std::to_string(distribution(rnd));
         };
         GenerateTests("tests_data/int/random_small", batches, raw_generator);
@@ -82,7 +92,7 @@ int main() {
         const auto batches = GenBatches(kMaxBatchSize, 1.2);
         std::uniform_int_distribution<int64_t> distribution(std::numeric_limits<int64_t>::min(),
                                                             std::numeric_limits<int64_t>::max());
-        auto raw_generator = [&rnd, &distribution]() -> std::string {
+        auto raw_generator = [&distribution]() -> std::string {
             return std::to_string(distribution(rnd));
         };
         GenerateTests("tests_data/int/random_big", batches, raw_generator);
--- libs/permute_algorithms/include/compression_sorts/lexicographic_sort.hpp
+++ libs/permute_algorithms/include/compression_sorts/lexicographic_sort.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "compression_sorts/range.hpp"
 #include "permute_interface.hpp"
 
 namespace CompressionSorts {
@@ -8,6 +9,23 @@ class LexicographicSortPermute : public IPermute {
 public:
     void GetPermutation(const Block& /*block*/, std::vector<size_t>& /*order*/) const override;
     std::string GetName() const override;
+};
+
+class LexicographicSortOfflineColumnOrderPermute : public IPermute {
+public:
+    void GetPermutation(const Block& /*block*/, std::vector<size_t>& /*order*/) const override;
+    std::string GetName() const override;
+};
+
+class LexicographicSortOnlineColumnOrderPermute : public IPermute {
+public:
+    void GetPermutation(const Block& /*block*/, std::vector<size_t>& /*order*/) const override;
+    std::string GetName() const override;
+
+private:
+    void GetPermutation(const Block& /*block*/, size_t /*processed_columns*/,
+                        std::vector<size_t> /*columns_order*/, Range /*range*/,
+                        std::vector<size_t>& /*order*/) const;
 };
 
 }  // namespace CompressionSorts
