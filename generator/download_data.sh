806f3cc6fdeeb60b4d18efd2b8c8fc72bb376476
Igor Markelov <ismarkelov@edu.hse.ru>
2024-05-12 12:32:03 +0000

    Create bash download data script

---
+++ generator/download_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir downloads
+
+# dictionary with english words
+wget --output-document=generator/downloads/dictionary.csv https://www.bragitoff.com/wp-content/uploads/2016/03/dictionary.csv
