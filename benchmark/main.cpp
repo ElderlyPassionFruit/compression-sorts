5cd4add86a3e550032da24a9c7a9601ab87ecf98
Igor Markelov <ismarkelov@edu.hse.ru>
2024-05-13 23:44:46 +0000

    Fix SparseTable usage

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -290,7 +290,7 @@ int main() {
     TestAllMannyIntegersColumnsTests<int8_t>("tests_data/int/many_random_small");
     TestAllSingleIntegersColumnTests<int64_t>("tests_data/int/random_big");
     TestViaStrings("tests_data/english/dictionary");
-    // TestViaStrings("tests_data/clickhouse/hits");
+    TestViaStrings("tests_data/clickhouse/hits");
     TestViaStrings("tests_data/clickhouse/dish");
     TestViaStrings("tests_data/clickhouse/menu");
     TestViaStrings("tests_data/clickhouse/menu_item");
