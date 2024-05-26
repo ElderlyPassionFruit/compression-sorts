f7660ab4f430375670040e71830540b0595555fc
Igor Markelov <ismarkelov@edu.hse.ru>
2024-05-26 14:00:44 +0000

    Prettify batches generator

--- libs/generator/src/batches.cpp
+++ libs/generator/src/batches.cpp
@@ -1,17 +1,17 @@
 #include "compression_sorts/batches.hpp"
 
 #include <cmath>
+#include <limits>
 
 namespace CompressionSorts {
 
 std::vector<size_t> GenerateBatches(BatchesSettings batches_settings) {
+    const auto [max_batch_size, exponent] = batches_settings;
     std::vector<size_t> batches;
-    for (size_t new_batch_size = 1; new_batch_size <= batches_settings.max_batch_size;
-         new_batch_size =
-             new_batch_size < batches_settings.max_batch_size
-                 ? std::min<size_t>(batches_settings.max_batch_size,
-                                    ceil(batches_settings.exponent * new_batch_size))
-                 : static_cast<size_t>(ceil(batches_settings.exponent * new_batch_size))) {
+    for (size_t new_batch_size = 1; new_batch_size <= max_batch_size;
+         new_batch_size = new_batch_size < max_batch_size
+                              ? std::min<size_t>(max_batch_size, ceil(exponent * new_batch_size))
+                              : std::numeric_limits<size_t>::max()) {
         batches.push_back(new_batch_size);
     }
     return batches;
